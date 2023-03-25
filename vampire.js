c07f740c909f3d4d76a5b807a89d4329ab8ed483
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-25 16:22:34 -0300

    completed addOffspring function, numberOfOffspring function, get numberOfVampiresFromOriginal function, and isMoreSentiorThan(vampire) function

--- vampire.js
+++ vampire.js
@@ -10,21 +10,42 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    const vampireLength = this.offspring.length
+    if (vampireLength === undefined || vampireLength === null) {
+      return 0;
+    }
+    // return this.offspring.length
+    return vampireLength;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.offspring === null) return false;
+    for (let v = 0; v < this.numberOfOffspring; v++) {
+     if (vampire.name === this.offspring[v].name) {
+       return true
+     }
+    }
+    return false
 
   }
 
@@ -39,6 +60,19 @@ class Vampire {
 
   }
 }
+
+const original = new Vampire("original")
+const ansel = new Vampire("ansel")
+const bart = new Vampire("bart")
+const elgort = new Vampire("elgort")
+const sarah = new Vampire("sarah")
+const andrew = new Vampire("andrew")
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
 
 module.exports = Vampire;
 
